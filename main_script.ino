e5f4c3bf0cdf5108e60af074a4d5886facd83f9d
Daniel <danielrportillo@gmail.com>
2025-04-10 13:16:03 -0500

    Update main_script.ino

--- main_script.ino
+++ main_script.ino
@@ -1,3 +1,20 @@
+// interactions
+int entrancePin = 2;
+int faceCaesarPin = 3;
+int killPin = 4;
+int backCaesarPin = 5;
+int exitPin = 6;
+
+// servos
+int door1Pin = 7;
+int caesarPin = 8;
+int door2Pin = 9;
+
+// LEDs
+int interiorLightPin = 10;
+int killLightPin = 11;
+
+
 void setup() {
   // put your setup code here, to run once:
 
